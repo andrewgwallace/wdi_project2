ab008bd94903903609954a06d1d9b8ec2e3632df
Crustacean87! <declanv@gmail.com>
2014-06-13 11:47:25 -0400

    made changes to questions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -9,16 +9,14 @@ class QuestionsController < ApplicationController
 
   def create
 
-    # if the question is a yes or no question, then create the question setting option_1 to yes, option_2 to no.
-    if params[:question][:range_question].nil? #!= nil
       Question.create(question_attributes)
-
 
   end
 
+private
 
    def question_attributes
-      params(:question).permit(:range_question, :option_1, :option_2)
+      params(:question).permit(:content, :option_1, :option_2)
     end
 
 end
