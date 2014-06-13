6a720cb2e3bbc859c73aeb377e0f1b22ce481e91
Crustacean87! <declanv@gmail.com>
2014-06-13 15:42:16 -0400

    Question create functionality now working. Veeery basic questions index.html.erb page working

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,23 +1,36 @@
 class QuestionsController < ApplicationController
 
+  before_action :authenticate_user!
+  # user_signed_in?
+
   def index
 
   # Including @user in order to create questions with a user_id params
- @user= User.find(current_user.id)
+  @user= User.find(current_user.id)
+  @questions = Question.all
+  @question = Question.new
 
+  end
+
+  def new
+    @question = Question.new
   end
 
   def create
 
-      Question.create(question_attributes)
+     @question = Question.create(question_attributes)
 
-      redirect_to  "/questions/#index"
+      redirect_to "/questions/#index"
+  end
+
+  def show
+    @questions = Question.all
   end
 
 private
 
    def question_attributes
-      params(:question).permit(:user_id, :content, :option_1, :option_2, :range_min, :range_max)
+      params.require(:question).permit(:user_id, :content, :option_1, :option_2, :range_min, :range_max)
     end
 
 end
